3aaf92148acf87b8e893375321d58cc8379b8674
masudeecs <44656186+masudeecs@users.noreply.github.com>
2024-04-17 23:47:11 +0600

    Add files via upload

---
+++ src/textSummarizer/entity/__init__.py
@@ -0,0 +1,12 @@
+# Entity is nothing but return type of a function
+
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
